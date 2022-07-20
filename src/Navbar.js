231aa83699b282cf961f1c6c3a69c36e0798ae97
Devin Knight <devin.knight@accenture.com>
2022-07-20 12:47:20 -0400

    added style attribute to Anchor tag  within Navbar as an example of inline styles (not something that's practical, but just coding an example.

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,7 +5,11 @@ const Navbar = () => {
             <h1>The React Test Blog</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create">New Entry</a>
+                <a href="/create" style={{
+                    color: 'white',
+                    backgroundColor: "#f1256d",
+                    borderRadius: '0.5rem'
+                }}>New Entry</a>
             </div>
         </nav>
     )
