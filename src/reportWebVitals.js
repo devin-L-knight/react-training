9f1f55f5fe7bb7a854f2d156a5c1e1f261ec6eb9
Devin Knight <devin.knight@accenture.com>
2022-07-20 11:17:51 -0400

    first-commit

---
+++ src/reportWebVitals.js
@@ -0,0 +1,15 @@
+// a performance file (don't really need...commenting out for time being)
+
+// const reportWebVitals = onPerfEntry => {
+//   if (onPerfEntry && onPerfEntry instanceof Function) {
+//     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+//       getCLS(onPerfEntry);
+//       getFID(onPerfEntry);
+//       getFCP(onPerfEntry);
+//       getLCP(onPerfEntry);
+//       getTTFB(onPerfEntry);
+//     });
+//   }
+// };
+
+// export default reportWebVitals;
