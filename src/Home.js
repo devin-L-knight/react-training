2b3d5380c741ba2de4ef644228df96b3d3ba8dab
Devin Knight <devin.knight@accenture.com>
2022-07-20 13:12:02 -0400

    drafted some examples of using the react hook (useState) to see how that updates values to the page based on some page interaction (the handleClick).

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,21 @@
+import { useState } from 'react'; // imports a react hook. (allows us to update a reactive value that is rendered on the page).
+
 // this is a stateless functional component
 const Home = () => {
 
-    const handleClick = () => {
-        console.log('CLICKED');
-    }
+    const [name, setName] = useState('Bob')
+    const [age, setAge] = useState(45);
 
-    const handleClickAgain = (name) => {
-        console.log(`Hello ${name}`);
+    const handleClick = (e) => {
+        setName('Jimmy')
+        setAge(30);
     }
 
     return (
         <div className="home">
             <h2>Home Page</h2>
+            <p>{ name } is { age } years old.</p>
             <button onClick={handleClick}>Click Me</button>
-            <button onClick={() => {
-                handleClickAgain('Bob')
-            }}>Click me again</button>
         </div>
     )
 }
