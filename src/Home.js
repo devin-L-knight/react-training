2d421a55b96d7d0a7dc344c00d206c861ce1221d
Devin Knight <devin.knight@accenture.com>
2022-07-20 12:12:54 -0400

    created basic home page component to build out.

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+// this is a stateless functional component
+const Home = () => {
+
+    return (
+        <div className="home">
+            <h2>Home Page</h2>
+        </div>
+    )
+}
+
+export default Home
