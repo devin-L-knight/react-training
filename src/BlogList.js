8804c01117025d78d7b4b143a5ae121dbb36867f
Devin Knight <devin.knight@accenture.com>
2022-07-20 17:33:19 -0400

    Added React Router Link Component to handle dynamic blog / article IDs.

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 
 // stateless functional component (uses the props passed down from higher components)
 const BlogList = (props) => {
@@ -9,8 +10,10 @@ const BlogList = (props) => {
             <h2>{title}</h2>
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>Written by: { blog.author }</p>
+                    <Link to={`/blogs/${blog.id}`}>
+                        <h2>{ blog.title }</h2>
+                        <p>Written by: { blog.author }</p>
+                    </Link>
                 </div>
             ))}
         </div>
