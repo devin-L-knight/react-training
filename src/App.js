3408eda69ba8a4b2cfc5a114152e812aa473edd3
Devin Knight <devin.knight@accenture.com>
2022-07-20 15:59:08 -0400

    Updated App.js to use React Router (Router, Route and Switch Components) to begin handling additional site pages.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 // NOTE: With the latest version of react, we do not need to import React from react like we did previously.
 import logo from './logo.svg';
 // import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// Site Components:
 import Navbar from './Navbar';
 import Home from './Home';
+import CreateBlog from './Create';
 
 function App() {
   const title = 'Welcome to the new React blog'
@@ -10,31 +14,36 @@ function App() {
   const link = 'http://www.google.com'
 
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+    <Router>
+      <div className="App">
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header> */}
 
-      <Navbar />
-      <div className="content">
-        {/* <h1>{title}</h1>
-        <p>Liked {likes} times</p>
+        <Navbar />
+        <div className="content">
+          {/* <h1>{title}</h1>
+          <p>Liked {likes} times</p>
 
-        <a href={link}>Google Site</a> */}
-        <Home />
+          <a href={link}>Google Site</a> */}
+          <Switch>
+            <Route exact path="/"><Home /></Route>
+            <Route path="/create"><CreateBlog /></Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
