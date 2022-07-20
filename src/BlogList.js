f69a37dc715d1262488833b45b7dbd4fd03c91fc
Devin Knight <devin.knight@accenture.com>
2022-07-20 14:56:16 -0400

    Added some functionality to the useEffect React hook to fetch data from a mock server (on port 8000) and updating state depending on whether the fetch request has an error or was successful.

--- src/BlogList.js
+++ src/BlogList.js
@@ -11,7 +11,6 @@ const BlogList = (props) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{ blog.title }</h2>
                     <p>Written by: { blog.author }</p>
-                    <button onClick={ () => { props.handleDelete(blog.id) } }>delete blog</button>
                 </div>
             ))}
         </div>
--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,11 @@
-import { useState } from 'react'; // imports a react hook. (allows us to update a reactive value that is rendered on the page).
+import { useState, useEffect } from 'react'; // imports a react hook. (allows us to update a reactive value that is rendered on the page).
 import BlogList from './BlogList';
 // this is a stateless functional component
 const Home = () => {
 
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'Bob', id: 1},
-        { title: 'The welcome party!', body: 'lorem ipsum...', author: 'Jimmy', id: 2},
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Jane', id: 3},
-        { title: 'Training with React', body: 'lorem ipsum...', author: 'Bob', id: 4}
-    ])
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setisPending] = useState(true);
+    const [error, setError] = useState(null);
 
     const [name, setName] = useState('Bob')
     const [age, setAge] = useState(45);
@@ -18,21 +15,51 @@ const Home = () => {
         setAge(30);
     }
 
-    const handleDelete = (id) => {
-        // we're going to create a new, filtered array.
-        // This approach means the original state remains unchanged.
-        const newBlogs = blogs.filter(blog => blog.id !== id);;
+    useEffect(() => {
+        // this setTimeout is just to simulate the time that would usually be spent fetching data from a database (shows our 'Loading...' message while the data would be loading)
+        setTimeout(() => {
 
-        setBlogs(newBlogs); // uses a hook via useState to update values of the new array
-    }
+            // we make a fetch request to our DB here for our local host (the port where the data is being served)
+            fetch('http://localhost:8000/blogs')
+                .then( res => {
+                    // console.log(res);
+                    if (!res.ok) {
+                        throw Error('Unable to fetch data for this resource')
+                    }
+                    // we will need to parse our response into a JSON
+                    return res.json()
+                })
+                .then( data => {
+                    // here we get the data from the fetch response
+                    setBlogs(data); // sets the blogs based on the fetched data
+                    setisPending(false);    // updates the pending message state to be false
+                    setError(null);
+                })
+                .catch( error => {
+                    setError(error.message);  // update our state to include the error message
+                    setisPending(false);    // updates the pending message state to be false
+
+                    // here we'll console out any kind of network error
+                    console.log(error.message);
+                })
+
+        }, 1500)
+
+    }, []);
 
     return (
         <div className="home">
             <h2>Home Page</h2>
             <p>{ name } is { age } years old.</p>
             <button onClick={handleClick}>Click Me</button>
-            <BlogList blogsEntries={blogs} title="All Blogs!" handleDelete={handleDelete}/>
-            <BlogList blogsEntries={blogs.filter((blog) => blog.author === 'Bob')} title="Bob's Blogs:" handleDelete={handleDelete} />
+
+            { error && <div>{error}</div>}
+            { isPending && <div>Loading...</div> }
+            { blogs && <BlogList blogsEntries={blogs} title="All Blogs!" /> }
+
+            {/* <BlogList blogsEntries={blogs.filter((blog) => blog.author === 'Bob')} title="Bob's Blogs:" handleDelete={handleDelete} /> */}
+            <button onClick={() => setName('Jimmy-John')}>change name</button>
+            <p>{ name }</p>
         </div>
     )
 }
