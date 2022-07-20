9aa6c80c4ee23aadef34d01510defa3e47a0830e
Devin Knight <devin.knight@accenture.com>
2022-07-20 12:49:26 -0400

    created draft handleClick function to the Home component and added it to a placeholder button.

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,14 @@
 // this is a stateless functional component
 const Home = () => {
 
+    const handleClick = () => {
+        console.log('CLICKED');
+    }
+
     return (
         <div className="home">
             <h2>Home Page</h2>
+            <button onClick={handleClick}>Click Me</button>
         </div>
     )
 }
