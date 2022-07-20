5ded6b3aa85905b04028d4a231ddaa78c573ddd6
Devin Knight <devin.knight@accenture.com>
2022-07-20 13:27:05 -0400

    added some blog previews to the home page and associated styles to help them stick out.

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,12 @@ import { useState } from 'react'; // imports a react hook. (allows us to update 
 // this is a stateless functional component
 const Home = () => {
 
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'Bob', id: 1},
+        { title: 'The welcome party!', body: 'lorem ipsum...', author: 'Jimmy', id: 2},
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Jane', id: 3}
+    ])
+
     const [name, setName] = useState('Bob')
     const [age, setAge] = useState(45);
 
@@ -16,6 +22,12 @@ const Home = () => {
             <h2>Home Page</h2>
             <p>{ name } is { age } years old.</p>
             <button onClick={handleClick}>Click Me</button>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by: { blog.author }</p>
+                </div>
+            ))}
         </div>
     )
 }
