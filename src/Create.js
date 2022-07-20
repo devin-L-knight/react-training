a214c620b7735c154312a0b6c2396786cf6c6d6d
Devin Knight <devin.knight@accenture.com>
2022-07-20 18:24:10 -0400

    preliminary state and additional functionality added to Create Article form in the Create component.

--- src/Create.js
+++ src/Create.js
@@ -1,4 +1,12 @@
+import { useState } from 'react';
+
 const CreateBlog = () => {
+
+    // we need to set-up some state to track the title and data that is entered into our form.
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Bob')
+
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
@@ -8,21 +16,30 @@ const CreateBlog = () => {
                 <input
                     type="text"
                     required
+                    value={title}
+                    onChange={(evt) => setTitle(evt.target.value)}
                 />
 
                 <label>Article body:</label>
 
                 <textarea
                     required
+                    onChange={(evt) => setBody(evt.target.value)}
                 ></textarea>
 
                 <label>Blog author:</label>
-                <select>
+                <select
+                    value={author}
+                    onChange={(evt) => setAuthor(evt.target.value)}
+                >
                     <option value="Bob">Bob</option>
                     <option value="Jimmy">Jimmy</option>
                     <option value="Jane">Jane</option>
                 </select>
                 <button>Post Article</button>
+                {/* <p>Draft Title:  {title}</p>
+                <p>Selected Author: {author}</p>
+                <p>Draft Text: {body}</p> */}
             </form>
         </div>
     )
