054438ab35d5bf372d45f2ccfa9bb7c00dbf821c
Devin Knight <devin.knight@accenture.com>
2022-07-20 18:12:06 -0400

    built out a form template to create a new article.

--- src/Create.js
+++ src/Create.js
@@ -2,6 +2,28 @@ const CreateBlog = () => {
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form>
+                <label>Blog title: </label>
+
+                <input
+                    type="text"
+                    required
+                />
+
+                <label>Article body:</label>
+
+                <textarea
+                    required
+                ></textarea>
+
+                <label>Blog author:</label>
+                <select>
+                    <option value="Bob">Bob</option>
+                    <option value="Jimmy">Jimmy</option>
+                    <option value="Jane">Jane</option>
+                </select>
+                <button>Post Article</button>
+            </form>
         </div>
     )
 }
