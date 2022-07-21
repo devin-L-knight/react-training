47a7cb01cb49ae0d3a36ceb7030f3621ffd78d9d
Devin Knight <devin.knight@accenture.com>
2022-07-21 10:50:12 -0400

    Restructured styles to be in specific styles folder & updated associated file imports to accomodate directory adjustment.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals'; // commented this out (this is a performance file brought in with react app...keepin in place for now).
 
