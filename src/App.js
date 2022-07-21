fa14368d188beb2c25c994ec4c49561c96164df7
Devin Knight <devin.knight@accenture.com>
2022-07-21 10:31:36 -0400

    Removed App.css file that was no longer needed and cleaned up some placeholder react jsx that was in place in App.js.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 // NOTE: With the latest version of react, we do not need to import React from react like we did previously.
 // import logo from './logo.svg';
-// import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // Site Components:
@@ -18,21 +17,6 @@ function App() {
   return (
     <Router>
       <div className="App">
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header> */}
-
         <Navbar />
         <div className="content">
           {/* <h1>{title}</h1>
